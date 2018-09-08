de04aafd2e4c48f508716a84e5479e86b7834b6c
Arthur J. DiLeo <aj.dileo@gmail.com>
2018-09-08 01:03:34 -0400

    prelim med form code

---
+++ ResSave/ResSave/MedViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MedViewController.swift
+//  ResSave
+//
+//  Created by Arthur DiLeo on 9/7/18.
+//  Copyright © 2018 Gregory. All rights reserved.
+//
+
+import UIKit
+
+class MedViewController: UIViewController {
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var bday: UITextField!
+    @IBOutlet weak var cond: UITextField!
+    @IBOutlet weak var notes: UITextField!
+    @IBOutlet weak var allerg: UITextField!
+    @IBOutlet weak var meds: UITextField!
+    @IBOutlet weak var weight: UITextField!
+    @IBOutlet weak var height: UITextField!
+    
+    let userDefaults = UserDefaults.standard
+    userDefaults.set(name, forKey: "name")
+    userDefaults.set(bday, forKey: "bday")
+    userDefaults.set(cond, forKey: "cond")
+    userDefaults.set(notes, forKey: "notes")
+    userDefaults.set(allerg, forKey: "allerg")
+    userDefaults.set(meds, forKey: "meds")
+    userDefaults.set(weight, forKey: "weight")
+    userDefaults.set(height, forKey: "height")
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func submit(_ sender: UIButton) {
+
+        print(userDefaults.string(forKey: "name"))
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
