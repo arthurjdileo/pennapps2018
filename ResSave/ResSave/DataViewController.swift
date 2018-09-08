212a6363bff0722c4c52daf020e6f7337e5f3343
Gregory Beitler <beitlergreg@gmail.com>
2018-09-08 02:54:11 -0400

    prelim write to txt file
    aj

--- ResSave/ResSave/DataViewController.swift
+++ ResSave/ResSave/DataViewController.swift
@@ -14,7 +14,8 @@ class DataViewController: UIViewController {
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
 
-
+    @IBOutlet weak var panic: UIButton!
+    
     @IBOutlet weak var pressed: UIButton!
     
     
@@ -29,6 +30,32 @@ class DataViewController: UIViewController {
         self.dataLabel!.text = dataObject
     }
 
-
+    @IBAction func execPanic(_ sender: Any) {
+        super.viewDidLoad()
+        let hello = "hello how are you"
+        let filename = "text"
+        let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        let fileURL = DocumentDirURL.appendingPathComponent(filename).appendingPathExtension("txt")
+        print("File Path: \(fileURL.path)")
+        do {
+            try hello.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print("failed to write")
+            print(error)
+        }
+        
+        var readString = ""
+        do {
+            readString = try String(contentsOf: fileURL)
+        } catch let error as NSError{
+            print("Failed to read file")
+            print(error)
+        }
+        
+        print("contents: \(readString)")
+    
+    }
+    
 }
 
--- ResSave/ResSave/MedViewController.swift
+++ ResSave/ResSave/MedViewController.swift
@@ -10,27 +10,6 @@ import UIKit
 
 class MedViewController: UIViewController {
 
-<<<<<<< Updated upstream
-//    @IBOutlet weak var name: UITextField!
-//    @IBOutlet weak var bday: UITextField!
-//    @IBOutlet weak var cond: UITextField!
-//    @IBOutlet weak var notes: UITextField!
-//    @IBOutlet weak var allerg: UITextField!
-//    @IBOutlet weak var meds: UITextField!
-//    @IBOutlet weak var weight: UITextField!
-//    @IBOutlet weak var height: UITextField!
-//
-//    let userDefaults = UserDefaults.standard
-//    userDefaults.set(name, forKey: "name")
-//    userDefaults.set(bday, forKey: "bday")
-//    userDefaults.set(cond, forKey: "cond")
-//    userDefaults.set(notes, forKey: "notes")
-//    userDefaults.set(allerg, forKey: "allerg")
-//    userDefaults.set(meds, forKey: "meds")
-//    userDefaults.set(weight, forKey: "weight")
-//    userDefaults.set(height, forKey: "height")
-
-=======
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var bday: UITextField!
     @IBOutlet weak var cond: UITextField!
@@ -41,9 +20,6 @@ class MedViewController: UIViewController {
     @IBOutlet weak var height: UITextField!
     
     let userDefaults = UserDefaults.standard
-    
-    
->>>>>>> Stashed changes
     
     
     
@@ -65,13 +41,6 @@ class MedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-<<<<<<< Updated upstream
-//    @IBAction func submit(_ sender: UIButton) {
-//
-//        print(userDefaults.string(forKey: "name"))
-//        
-//    }
-=======
     @IBAction func submit(_ sender: UIButton) {
 
         userDefaults.set(name, forKey: "name")
@@ -84,7 +53,6 @@ class MedViewController: UIViewController {
         userDefaults.set(height, forKey: "height")
         
     }
->>>>>>> Stashed changes
     
     /*
     // MARK: - Navigation
